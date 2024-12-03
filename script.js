027a3a1cc18769507ffdf7a4e77c79a16e16d71a
Kumudha03 <kumudhalakshmi726@gmail.com>
2024-12-03 04:46:26 -0800

    modified

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load",()=>{
+
+    function handleIntersection(entries,observer){
+        entries.forEach(entry=>{
+            if(entry.isIntersecting){
+            entry.target.classList.add("visible")};
+        });
+    };
+
+    const options={root:null,threshold:0.2};
+
+    const observer =new IntersectionObserver(handleIntersection,options);
+
+    const sections=document.querySelectorAll(div);
+    sections.forEach(section=>
+        observer.observe(section));
+
+});
